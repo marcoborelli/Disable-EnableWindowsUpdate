03e33f60529b13a87bca1cc5c68d2a656fcf4c59
Marco Borelli <101565126+marcoborelli@users.noreply.github.com>
2022-10-23 19:05:48 +0200

    Version 1

---
+++ DisableEnable.bat
@@ -0,0 +1,49 @@
+@echo off
+color a
+set /p input= "VUOI DISATTIVARE [INVIO] O ATTIVARE GLI AGGIORNAMENTI? "
+if %input%a==a (
+	goto disabilita
+)
+
+goto abilita
+
+
+
+:abilita
+
+	reg copy HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate.old /f /s
+	reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f
+	sc config wuauserv start=auto 
+
+	exit
+
+:disabilita
+	
+	goto creazioneTempFile
+	:dentroDisabilita
+	"wu.reg"
+	sc config wuauserv start=disabled
+
+	del wu.reg
+	
+	exit
+
+
+
+
+:creazioneTempFile
+
+	echo Windows Registry Editor Version 5.00 > wu.reg
+	echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate] >> wu.reg
+	echo "WUServer"="fakeup1" >> wu.reg
+	echo "WUStatusServer"="fakeupd1" >> wu.reg
+	echo "UpdateServiceUrlAlternate"="fakeupd2" >> wu.reg
+	echo "DoNotConnectToWindowsUpdateInternetLocations"=dword:00000001 >> wu.reg
+	echo "DoNotConnectToWindowsUpdateInternetLocations"=dword:00000001 >> wu.reg
+	echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU] >> wu.reg
+	echo "NoAutoRebootWithLoggedOnUsers"=dword:00000001 >> wu.reg
+	echo "UseWUServer"=dword:00000001 >> wu.reg
+
+goto dentroDisabilita
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# DISABLE OR ENABLE WINDOWS UPDATE
+
+If you want to disable W.U. it adds some values to registry and it disables the wuauserv service.\
+If you want to re-enable W.U. it rename the folder in the registry and it re-enable the wuauserv service.
